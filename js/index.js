8d661880884f02e59f3f103ce133f8f0ed1b7087
SANmq <50514663+SANmq@users.noreply.github.com>
2020-04-20 13:38:28 +0800

    轮播图初步成功

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
-var app = {
-    
+var el = {
+    img: document.getElementsByClassName("img-player")[0]
 }
+
+var p = 520
+el.img.style.left = -520 + "px";
+
+setInterval(function view() {
+
+    p += -520
+    el.img.style.left = p + "px";
+    if (p<=-520*4){
+        p=520
+    }
+},2000)
+
+
+// position: absolute; overflow: hidden; width: 3640px; transition-duration: 0.3s; transform: translate3d(-1560px, 0px, 0px); backface-visibility: hidden; left: 0px; opacity: 1;
