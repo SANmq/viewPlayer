dc6969e61751ee093d24557d495d4fc285318a25
SANmq <50514663+SANmq@users.noreply.github.com>
2020-04-21 13:28:29 +0800

    中午基本啥都没改
    准备想办法封装一个轮播图的方法，这周完成这项工作，细节针对

--- js/index.js
+++ js/index.js
@@ -1,18 +1,37 @@
-var el = {
-    img: document.getElementsByClassName("img-player")[0]
+var diamond = {
+    init : function(){  //初始化函数
+        this.initData()
+    },
+
+    initData : function(){  //初始化数据函数
+        this.app = document.getElementById("view-circle");
+        this.img = this.app.getElementsByClassName('img-player')
+    },
+
+    haddle : function(){ //事件监听函数
+
+    },
+
+
+
 }
 
-var p = 520
-el.img.style.left = -520 + "px";
-
-setInterval(function view() {
-
-    p += -520
-    el.img.style.left = p + "px";
-    if (p<=-520*4){
-        p=520
-    }
-},2000)
 
 
-// position: absolute; overflow: hidden; width: 3640px; transition-duration: 0.3s; transform: translate3d(-1560px, 0px, 0px); backface-visibility: hidden; left: 0px; opacity: 1;
+
+
+// var el = {
+//     img: document.getElementsByClassName("img-player")[0]
+// }
+
+// var p = 520
+// el.img.style.left = -520 + "px";
+
+// setInterval(function view() {
+
+//     p += -520
+//     el.img.style.left = p + "px";
+//     if (p<=-520*4){
+//         p=520
+//     }
+// },2000)
