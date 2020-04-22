ee7285ddd853560323cbcc52a40d3d2cd7fd79ef
SANmq <50514663+SANmq@users.noreply.github.com>
2020-04-22 13:37:43 +0800

    中午更新

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 var diamond = {
     init: function () {  //初始化函数
         this.initData();
-        this.moveStart()
+        this.moveStart();
+        this.handle();
     },
 
     initData: function () {  //初始化数据函数
@@ -12,17 +13,30 @@ var diamond = {
     },
 
     handle: function () { //事件监听函数
-
+        this.handleOnmouseover()
+    },
+    handleOnmouseover (){
+        self = this;
+        this.img.onmouseover = function(e){
+            console.log('我被悬浮了')
+            console.log(e)
+            clearInterval(self.a1)
+        }
+        this.img.onmouseout = function(e){
+            console.log('我移开了')
+            console.log(e)
+            self.moveStart()
+        }
     },
 
     moveStart: function(){
         var self = this
-        setInterval(function () {
+        this.a1 = setInterval(function () {
             self.position += -520;
             self.img.style.left = self.position + 'px';
             if (self.position <= -520 * 4) {
                 self.position = 520;
             }
-        }, 2000)
+        }, 100)
     },
 }
