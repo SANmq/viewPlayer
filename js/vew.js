c0d691c8db6ee0e2c78da80dd746f52132ad0f20
SANmq <739699025@qq.com>
2020-04-23 23:28:28 +0800

    轮播成功
    除了一个error但是又没了，就是代码比较冗杂，其他都ok

--- js/vew.js
+++ js/vew.js
@@ -1,30 +1,65 @@
 var viewCircle = {
 
-    init: function (el, width, height) {
+    init: function (el) {
         // 定义初始的展示位置大小,没有填充默认大小
-        this.width = width || '520px';
-        this.height = height || '280px';
-        this.creatHtml(el)
-        console.log(el.style)
-        this.initData(el)
-    },
-    initData: function () {
-        // 获取需要的初始元素或其它数据内容
-
-
+        this.initData(el);
+        this.active();
     },
 
-    handle: function () {
+    initData: function (el) {
+        this.width = 520;
+        this.height = 280;
+        this.times = 1000;
+        this.exhibition = el.getElementsByClassName("img-player")[0];
+        this.controlBar = el.getElementsByClassName('select-nav')[0];
+        this.leftButton = el.getElementsByClassName('select-left')[0];
+        this.rightButton = el.getElementsByClassName('select-right')[0];
+        this.index = 0;
+        this.length = this.creatHtml();
+        this.li = this.controlBar.getElementsByTagName("li");
+        this.li[this.index].className = 'select';
+        this.autoMove(this.times);
+    },
+
+    handle: function (el) {
         // 获取元素的监听状态
+        console.log(this.rightButton);
     },
 
-    autoMove: function () {
+    active: function () {
+        self = this;
+        this.exhibition.onmouseout = function (e) {
+            self.autoMove(self.times);
+        };
+        this.exhibition.onmouseover = function (e) {
+            clearInterval(self.auto);
+        };
+    },
+
+    autoMove: function (time) {
         // 控制轮播图的移动，修改属性
+        var self = this
+        this.auto = setInterval(function () {
+            let x = -1 * self.index * self.width + 'px'
+            self.exhibition.style.transform = `translate3d(${x}, 0px, 0px)`;
+            for (let i in self.li) {
+                if (i == self.index) {
+                    self.li[i].className = 'select';
+                } else {
+                    self.li[i].className = '';
+                }
+            }
+            if (self.index == self.length - 1) {
+                self.index = 0;
+            } else {
+                self.index += 1;
+            }
+        }, time)
     },
 
-    creatHtml: function (el) {
-        // 根据数据产生对应的html代码内容
-        // el.innerHTML
+    creatHtml: function () {
+        // 根据数据产生对应的html代码内容,修改部分样式
+
         var data = {
             0: {
                 url: "img/img1.webp"
@@ -42,35 +77,22 @@ var viewCircle = {
                 url: "img/img5.webp"
             }
         };
-
-        
-
-
-
-        let temp = `
-        <div class="img-player">
-            <a href="" class="img"><img src="img/img1.webp" alt=""></a>
-            <a href="" class="img"><img src="img/img2.jpg" alt=""></a>
-            <a href="" class="img"><img src="img/img3.jpg" alt=""></a>
-            <a href="" class="img"><img src="img/img4.jpg" alt=""></a>
-            <a href="" class="img"><img src="img/img5.webp" alt=""></a>
-        </div>
-        <a class="select-left" href="javascript:void(0)">&lt;</a>
-        <a class="select-right" href="javascript:void(0)">&gt;</a>
-        <ul class="select-nav justify">
-            <li><a href="javascript:void(0)"></a></li>
-            <li><a href="javascript:void(0)"></a></li>
-            <li><a href="javascript:void(0)"></a></li>
-            <li><a href="javascript:void(0)"></a></li>
-            <li><a href="javascript:void(0)"></a></li>
-        </ul>
-        `
+        let img = "",
+            but = "",
+            length = 0;
+        for (let i in data) {
+            img += `<a href="" class="img"><img src=${data[i].url} alt=""></a>\n`
+            but += `<li><a href="javascript:void(0)"></a></li>`
+            length += 1;
+        }
+        this.exhibition.innerHTML = img;
+        this.controlBar.innerHTML = but;
+        this.controlBar.style.width = length * 20 + 'px';
+        return length
     },
     user: function () {
         // 控制用户行为
     },
-
-
 
 
 }
