ff86caf93dd75bf309937049bc59b9a5b068a0c7
SANmq <50514663+SANmq@users.noreply.github.com>
2020-04-30 13:50:46 +0800

    Create myToolBox.js

---
+++ js/myToolBox.js
@@ -0,0 +1,35 @@
+var myToolBox = {
+
+    // 兼容性方法
+    // 给元素添加监听事件
+    addEvent: function (elem, type, handle) {
+        if (elem.addEventListener) {
+            elem.addEventListener(type, handle, false);
+        } else if (elem.attachEvent) {
+            elem.attachEvent('on' + type, function () {
+                handle.call(elem);
+            })
+        } else {
+            elem['on' + type] = handle;
+        }
+    },
+
+    // 停止元素事件冒泡
+    stopBubble: function (event) {
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    },
+
+    // 清除元素（浏览器）默认事件
+    cancelHandler(event) {
+        if (event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnVlaue = false;
+        }
+    }
+
+}
