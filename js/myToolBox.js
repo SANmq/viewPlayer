58bcc1eb564c7f4db5b59118b437e11401ca861d
SANmq <739699025@qq.com>
2020-05-05 20:14:46 +0800

    工具更新

--- js/myToolBox.js
+++ js/myToolBox.js
@@ -30,6 +30,23 @@ var myToolBox = {
         } else {
             event.returnVlaue = false;
         }
-    }
+    },
 
+    loadScript(url, callback) {
+        let script = document.createElement('script');
+        script.type = "text/javascript";
+        if (script.readyState) {
+            script.onreadystatechange = function () {
+                if (script.readyState == "complete" || script.readyState == "loaded") {
+                    callback();
+                }
+            }
+        } else {
+            script.onload = function () {
+                callback();
+            }
+        }
+        script.src = url;
+        document.head.appendChild(script);
+    }
 }
